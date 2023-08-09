93a8117a5a89e93a8ab2cbcc28f2d6511f0f632e
juanerq <jrjuanreyes64@gmail.com>
2023-08-09 18:57:06 -0500

    remane bash

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const pathMount = '/home/desarrollo/solicitudes'
     if(fileExists) {
       console.log(`Rename: ${fileExists} => ${newNameFile}.mp3`)
 
-      exec(`sudo mv /home/desarrollo/solicitudes/01/04/20220104-080459_3108205685_DAVIVI09_M1040804590009946164-all.mp3 /home/desarrollo/solicitudes/01/04/cob_Lex_3108205685_1641301992000.mp3`, (error, stdout, stderr) => {
+      exec(`sudo mv ${pathFile}/${fileExists} ${pathFile}/${newNameFile}.mp3`, (error, stdout, stderr) => {
           if (error) {
               console.log(`error: ${error.message}`);
               return;
